b5dea9559b4d272b7ec99c196c7a819992a0b8cb
Marcus Zanona <zanona@me.com>
2016-06-25 15:39:16 +0100

    Refactor Binding system

--- index.js
+++ index.js
@@ -69,21 +69,53 @@ class CoreWebComponent extends HTMLElement {
 }
 
 class WebComponent extends CoreWebComponent {
-  _searchBindings(text) {
+  static getObj(base, path) {
+    if (path.match(/\./)) {
+      for (const key of path.split('.')) {
+        base = base[key];
+        if (typeof base === 'undefined') { break; }
+      }
+      return base;
+    }
+    return base[path];
+  }
+  static setObj(base, path, value) {
+    if (path.match(/\./)) {
+      const keys = path.split('.');
+      base = base[keys.shift()];
+      let key;
+      while ((key = keys.shift())) {
+        if (keys.length) {
+          // IF OBJ.NAME.FIRST DOESN'T EXIST, CREATE OBJ.NAME FIRST
+          base[key] = base[key] || {};
+          base = base[key];
+        }
+        if (!keys.length) {
+          base[key] = value;
+        }
+      }
+    }
+    return base[path] = value;
+  }
+  static searchBindings(text) {
     const tag = /([\[\{]){2}([a-z-\.\_$]+)[\]\}]{2}/gi,
           bindings = [];
-    text.replace(tag, (raw, type, key) => {
-      bindings.push({
-        auto: type === '{',
-        key,
-        raw
+    if (text && text.replace) {
+      text.replace(tag, (raw, type, key) => {
+        bindings.push({
+          auto: type === '{',
+          key,
+          raw
+        });
       });
-    });
+    }
     return bindings;
   }
-  _searchForHostComponent(node) {
-    while (node.parentNode) { node = node.parentNode; }
-    return node.host;
+  static searchForHostComponent(node) {
+    const parent = node.parentNode;
+    if (!parent) { return node.host; }
+    if (parent instanceof WebComponent) { return parent; }
+    return WebComponent.searchForHostComponent(parent);
   }
   _bind(node, binding) {
     let from, fromKey, to, toKey;
@@ -94,10 +126,7 @@ class WebComponent extends CoreWebComponent {
     if (node._ownerElement === this) {
       from     = node._ownerElement;
       fromKey  = node.nodeName;
-      // SOMETIMES COMPONENTS GET ADDED IN DIFFERENT ORDER
-      // INSTEAD OF RELYING ON NODE._OWNERINSTANCE WE
-      // NEED TO SEARCH THE DOM TREE UPWARDS
-      to       = this._searchForHostComponent(from);
+      to       = node._ownerInstance;
       toKey    = binding.key;
     } else {
       from     = node._ownerInstance;
@@ -127,43 +156,41 @@ class WebComponent extends CoreWebComponent {
     });
   }
   _registerProperties(node) {
-    const bindings = this._searchBindings(node.textContent);
+    const bindings    = WebComponent.searchBindings(node.textContent),
+          isComponent = node._ownerElement instanceof WebComponent,
+          isAttribute = node.nodeType === Node.ATTRIBUTE_NODE;
+
     for (const binding of bindings) { this._bind(node, binding); }
 
-    if (node.nodeType === Node.ATTRIBUTE_NODE && node._ownerElement instanceof WebComponent) {
-      //console.log('ASSIGNING', node.nodeName, '=' , node.textContent);
-      // DO NOT SET BINDING STRINGS
-      if (!node.textContent.match(/^[\{\[]{2}/)) {
-        node._ownerElement.set(node.nodeName, node.textContent);
-      }
+    if (isComponent && isAttribute) {
+      this._preSet(node._ownerElement, node.nodeName, node.textContent);
     }
   }
   _dig(node) {
-    if (node.nodeType === Node.TEXT_NODE) {
-      node._ownerElement = node.parentNode;
-      return this._registerProperties(node);
+    const INSTANCE = '_ownerInstance',
+          ELEMENT  = '_ownerElement';
+    if (!node.hasOwnProperty(INSTANCE)) {
+      Object.defineProperty(node, INSTANCE, {
+        value: WebComponent.searchForHostComponent(node)
+      });
     }
     if (node.attributes) {
       for (const attr of Array.from(node.attributes)) {
-        //console.log(this.nodeName, node.nodeName);
-        // Thanks for deprecating attr.ownerElement Mozilla!
-        attr._ownerElement = node;
-        if (node === this) {
-          attr._ownerInstance = this;
-        } else {
-          attr._ownerInstance = node._ownerInstance;
+        if (!attr.hasOwnProperty(INSTANCE)) {
+          Object.defineProperty(attr, ELEMENT,  { value: node });
+          Object.defineProperty(attr, INSTANCE, { value: node[INSTANCE] });
         }
         this._registerProperties(attr);
       }
     }
-    for (const child of Array.from(node.childNodes)) {
-      //Object.defineProperty(child, '_ownerInstance', {value: this});
-      child._ownerInstance = this;
-      this._dig(child);
+    if (node.nodeType === Node.TEXT_NODE) {
+      Object.defineProperty(node, ELEMENT, {value: node.parentNode});
+      this._registerProperties(node);
     }
+    Array.from(node.childNodes).forEach(this._dig.bind(this));
   }
   _analyse() {
-    console.log('--------', this.nodeName, '--------');
+    //console.log('--------', this.nodeName, '--------');
 
     this._dig(this);
     if (this.shadowRoot) { this._dig(this.shadowRoot); }
@@ -173,44 +200,30 @@ class WebComponent extends CoreWebComponent {
       this._updateListenerValues(key, this._bindings[key]);
     }
   }
+  _preSet(related, relatedKey, value) {
+    const rValue          = WebComponent.getObj(related, relatedKey),
+          valueExists     = typeof value  !== 'undefined',
+          valuesDiffer    = value !== rValue,
+          isValueTemplate = WebComponent.searchBindings(value).length;
+    if (valueExists && valuesDiffer && !isValueTemplate) {
+      related.set(relatedKey, value);
+    }
+  }
+  _updateListenerAttributeValue(listener) {
+    let content = listener.originalValue;
+    WebComponent.searchBindings(content).forEach((b) => {
+      content = content.replace(b.raw, (m) => {
+        return WebComponent.getObj(listener.host, b.key) || m;
+      });
+    });
+    listener.node.textContent = content;
+  }
   _updateListenerValues(key, keyListeners) {
     for (const listener of keyListeners) {
-      const related = listener.related;
       if (listener.related instanceof WebComponent) {
-        // TODO: COMPRESS FUNCTION / LESS NESTED
-        // TODO: REFACTOR CALLED LOGIC
-        if (this.called) {
-          delete this.called;
-          delete related.called;
-          this.connection += 1;
-          //console.log('ALREADY CALLED', listener.key, key);
-          if (this.connection >= 3) {
-            this.connection = 0;
-            //console.log('CALLING AGAIN', this.connection, this.conn);
-            const value = this.getObj(this, key);
-            if (typeof value !== 'undefined') {
-              related.set(listener.key, value);
-            }
-          }
-        } else {
-          related.called = true;
-          this.connection = 1;
-          // ONLY SET DEFINED VALUES;
-          /*eslint max-depth: [1,4]*/
-          const value = this.getObj(this, key);
-          if (typeof value !== 'undefined') {
-            related.set(listener.key, value);
-          }
-        }
+        this._preSet(listener.related, listener.key, WebComponent.getObj(this, key));
       }
-      //UPDATE ATTRIBUTES
-      let content = listener.originalValue;
-      this._searchBindings(content).forEach((b) => {
-        content = content.replace(b.raw, (m) => {
-          return this.getObj(listener.host, b.key) || m;
-        });
-      });
-      listener.node.textContent = content;
+      this._updateListenerAttributeValue(listener);
     }
   }
   _refreshDependentListeners(objName) {
@@ -221,36 +234,8 @@ class WebComponent extends CoreWebComponent {
       }
     });
   }
-  getObj(base, path) {
-    if (path.match(/\./)) {
-      for (const key of path.split('.')) {
-        base = base[key];
-        if (typeof base === 'undefined') { break; }
-      }
-      return base;
-    }
-    return base[path];
-  }
-  setObj(path, value) {
-    if (path.match(/\./)) {
-      const keys = path.split('.');
-      let base = this[keys.shift()],
-          key;
-      while ((key = keys.shift())) {
-        if (keys.length) {
-          // IF OBJ.NAME.FIRST DOESN'T EXIST, CREATE OBJ.NAME FIRST
-          base[key] = base[key] || {};
-          base = base[key];
-        }
-        if (!keys.length) {
-          base[key] = value;
-        }
-      }
-    }
-    return this[path] = value;
-  }
   set(key, value) {
-    this.setObj(key, value);
+    WebComponent.setObj(this, key, value);
     const keyListeners = this._bindings[key];
     if (keyListeners) { this._updateListenerValues(key, keyListeners); }
 
